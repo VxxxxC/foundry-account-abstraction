195d6ddf21a3a9b0b717e2a9036cad9ebad025de
vxxxxc <veper1234@icloud.com>
2026-02-02 00:41:41 +0800

    create execute functions, and related modifier & error

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -11,10 +11,15 @@ import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstrac
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
+    // INFO: ***ERRORS***
     error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes);
 
+    // INFO: ***STATE VARIABLES***
     IEntryPoint private immutable i_entryPoint;
 
+    // INFO: ***MODIFIERS***
     modifier requireFromEntryPoint() {
         if (msg.sender != address(i_entryPoint)) {
             revert MinimalAccount__NotFromEntryPoint();
@@ -22,25 +27,46 @@ contract MinimalAccount is IAccount, Ownable {
         _;
     }
 
+    modifier requireFromEntryPointOwner(){
+        if(msg.sender != address(i_entryPoint) && msg.sender != owner()){
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    // INFO: ***CONSTRUCTOR***
     constructor(address entryPoint) Ownable(msg.sender) {
         i_entryPoint = IEntryPoint(entryPoint);
     }
+
+    // INFO: ***FUNCTIONS***
+    receive() external payable {}
+
+    // INFO: ***EXTERNAL FUNCTIONS***
+
+    function execute(address destination, uint256 value, bytes calldata functionData) external requireFromEntryPointOwner {
+        (bool success, bytes memory result) = destination.call{value: value}(functionData);
+        if(!success){
+            revert MinimalAccount__CallFailed(result);
+        }
+     }
 
     // If it is Minimal account owner, signature will be valid
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         requireFromEntryPoint
-        returns (uint256 validationDate)
+        returns (uint256 validationData)
     {
         uint256 validationData = _validateSignature(userOp, userOpHash);
         _payPrefund(missingAccountFunds);
     }
 
+    // INFO: ***INTERNAL FUNCTIONS***
     // EIP-191 version of signed hash
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
         view
-        returns (uint256 validateionData)
+        returns (uint256 validationData)
     {
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
         address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
