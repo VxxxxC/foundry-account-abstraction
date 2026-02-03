b74ea4d4daa31651ec8915d5ae1e12b6daca87ee
vxxxxc <veper1234@icloud.com>
2026-02-03 14:15:42 +0800

    [TEST CASE] generate UserOp in test case

--- test/MinimalAccountTest.t.sol
+++ test/MinimalAccountTest.t.sol
@@ -6,11 +6,14 @@ import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
 import {DeployMinimal} from "../script/DeployMinimal.s.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp, PackedUserOperation} from "../script/SendPackedUserOp.s.sol";
 
 contract MinimalAccountTest is Test {
     HelperConfig helperConfig;
     MinimalAccount minimalAccount;
     ERC20Mock usdc;
+    SendPackedUserOp sendPackedUserOp;
+
     uint256 constant AMOUNT = 1e18;
 
     address randomUser = makeAddr("randomUser");
@@ -19,6 +22,7 @@ contract MinimalAccountTest is Test {
         DeployMinimal deployMinimal = new DeployMinimal();
         (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
         usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
     }
 
     // USDC Mint
@@ -27,6 +31,7 @@ contract MinimalAccountTest is Test {
     // USDC contract
     // come from entryPoint
     function testOwnerCanExecuteCommands() public {
+        // arrange
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
         uint256 value = 0;
         address destination = address(usdc);
@@ -39,6 +44,7 @@ contract MinimalAccountTest is Test {
     }
 
     function testNotOwnerCannotExecuteCommands() public {
+        // arrange
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
         uint256 value = 0;
         address destination = address(usdc);
@@ -48,4 +54,20 @@ contract MinimalAccountTest is Test {
         vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOwner.selector);
         minimalAccount.execute(destination, value, functionData);
     }
+
+    function testRecoverSignedOp() public {
+        // arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        uint256 value = 0;
+        address destination = address(usdc);
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector, destination, value, functionData);
+        PackedUserOperation memory packedUserOp = sendPackedUserOp.generateSignedUserOperation(executeCallData, helperConfig.getConfig());
+        // act
+
+        // assert
+    }
+
+    function testValidationOfUserOp() public {}
 }
